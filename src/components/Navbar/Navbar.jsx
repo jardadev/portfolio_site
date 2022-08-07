dee008302e793db9982162779bc84ab8cf81b9d3
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-08-07 12:53:05 -0700

    add navbar and navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { MdHome } from 'react-icons/md';
+import './Navbar.sass';
 
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<div className='navbar-ui p-2'>
+			<MdHome size={64} />
+			<ul className='navbar-links'>
+				<li>About</li>
+				<li>Projects</li>
+				<li>Contact</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Navbar;
