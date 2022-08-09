34fa04c784355da385cdf708bbc3a22abab16239
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-08-08 19:45:02 -0700

    rename Home to Content

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 
-const Home = () => {
+const Content = () => {
 	return (
 		<>
 			<Header />
@@ -9,4 +9,4 @@ const Home = () => {
 	);
 };
 
-export default Home;
+export default Content;
