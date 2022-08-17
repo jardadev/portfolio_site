6c32d24f6318f529ddb37638cff420077f680170
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-08-17 16:10:52 -0700

    refactor Header to Landing

---
+++ src/components/Landing/Landing.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import { BsLinkedin, BsTwitter, BsGithub } from 'react-icons/bs';
+import { motion } from 'framer-motion';
+import './Landing.sass';
+
+const Landing = () => {
+	return (
+		<motion.div
+			className='header-content'
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+		>
+			{/* TODO: Span className */}
+			<h1 className='header-title'>
+				Hello, I'm <span>Aaron Johnson</span>.
+			</h1>
+			<div>
+				<p>Full Stack Developer 💻</p>
+				<p>Problem Solver 🧮</p>
+				<p>Music Enthusiast 🎧</p>
+				<p>Lifelong Learner 📝</p>
+			</div>
+			<div>
+				<h6>Connect With Me:</h6>
+				<hr />
+				<div className='socials'>
+					<motion.a
+						whileHover={{ scale: 1.2 }}
+						whileTap={{ scale: 0.8 }}
+						href='https://www.linkedin.com/in/jardacs/'
+					>
+						<BsLinkedin size={24} />
+					</motion.a>
+					<motion.a
+						whileHover={{ scale: 1.2 }}
+						whileTap={{ scale: 0.8 }}
+						href='https://twitter.com/JardaDono'
+					>
+						<BsTwitter size={24} />
+					</motion.a>
+					<motion.a
+						whileHover={{ scale: 1.2 }}
+						whileTap={{ scale: 0.8 }}
+						href='https://github.com/jardaaron'
+					>
+						<BsGithub size={24} />
+					</motion.a>
+				</div>
+			</div>
+		</motion.div>
+	);
+};
+
+export default Landing;
