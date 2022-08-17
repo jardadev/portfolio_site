5b9b91c64c5ffde007f8330cd05a650b5ff3f504
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-08-17 16:14:12 -0700

    add framer motion components, refactor

---
+++ src/views/Content.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import MainContentContainer from '../containers/MainContentContainer';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Landing from '../components/Landing/Landing';
+import './Content.sass';
+
+const Content = () => {
+	const [defaultView, setDefaultView] = useState(true);
+	const [aboutView, setAboutView] = useState(false);
+	const [projectsView, setProjectsView] = useState(false);
+	const [contactView, setContactView] = useState(false);
+
+	// FIXME: Change function calls to '!view' so that I may click the view button to show component and again to hide component and show landing component
+	const setView = (view) => {
+		switch (view) {
+			case 'about':
+				setDefaultView(false);
+				setContactView(false);
+				setProjectsView(false);
+				setAboutView(true);
+				break;
+			case 'projects':
+				setDefaultView(false);
+				setAboutView(false);
+				setContactView(false);
+				setProjectsView(true);
+				break;
+			case 'contact':
+				setDefaultView(false);
+				setAboutView(false);
+				setProjectsView(false);
+				setContactView(true);
+				break;
+			case 'home':
+				setAboutView(false);
+				setProjectsView(false);
+				setContactView(false);
+				setDefaultView(true);
+				break;
+			default:
+				break;
+		}
+	};
+
+	return (
+		<MainContentContainer>
+			<div className='view-buttons'>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('home')}
+				>
+					home
+				</button>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('about')}
+				>
+					about
+				</button>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('projects')}
+				>
+					projects
+				</button>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('contact')}
+				>
+					contact
+				</button>
+			</div>
+			<Row>
+				<Col>
+					<img
+						className='cover-img'
+						src='/assets/images/cover.jpg'
+						alt='Aaron Johnson wearing a green shirt, smiling.'
+					/>
+				</Col>
+				<Col>
+					{defaultView && <Landing />}
+					{aboutView && <div>About</div>}
+					{projectsView && <div>Projects</div>}
+					{contactView && <div>Contact</div>}
+				</Col>
+			</Row>
+		</MainContentContainer>
+	);
+};
+
+export default Content;
