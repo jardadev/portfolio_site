ec213a1048f292a4e052712b27d7eb33996d0138
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-08-25 11:06:21 -0700

    refactor lift state to app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
+import React, { useState } from 'react';
 import AppLayout from './containers/AppLayout';
 import Content from './views/Content';
 import Navbar from './components/Navbar/Navbar.jsx';
 
 const App = () => {
+	const [defaultView, setDefaultView] = useState(true);
+	const [aboutView, setAboutView] = useState(false);
+	const [projectsView, setProjectsView] = useState(false);
+	const [contactView, setContactView] = useState(false);
+	const setView = (view) => {
+		switch (view) {
+			case 'about':
+				setDefaultView(false);
+				setContactView(false);
+				setProjectsView(false);
+				setAboutView(true);
+				break;
+			case 'projects':
+				setDefaultView(false);
+				setAboutView(false);
+				setContactView(false);
+				setProjectsView(true);
+				break;
+			case 'contact':
+				setDefaultView(false);
+				setAboutView(false);
+				setProjectsView(false);
+				setContactView(true);
+				break;
+			case 'home':
+				setAboutView(false);
+				setProjectsView(false);
+				setContactView(false);
+				setDefaultView(true);
+				break;
+			default:
+				break;
+		}
+	};
 	return (
 		<AppLayout>
-			<Navbar />
-			<Content />
+			<Navbar setView={setView} />
+			<Content
+				defaultView={defaultView}
+				aboutView={aboutView}
+				contactView={contactView}
+				projectsView={projectsView}
+			/>
 		</AppLayout>
 	);
 };
