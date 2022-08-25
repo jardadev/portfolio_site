657737325d22cd466b84127758da59a52e0bd430
Aaron Johnson <sugarwaterpurple.js@gmail.com>
2022-08-25 11:09:06 -0700

    add nav buttons to navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
-
 import './Navbar.sass';
 
-const Navbar = () => {
+const Navbar = ({ setView }) => {
 	return (
 		<div className='navbar-ui'>
 			<h1 className='navbar-header'>Jarda.dev</h1>
+			<div className='view-buttons'>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('home')}
+				>
+					home
+				</button>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('about')}
+				>
+					about
+				</button>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('projects')}
+				>
+					projects
+				</button>
+				<button
+					className='btn btn-primary'
+					onClick={() => setView('contact')}
+				>
+					contact
+				</button>
+			</div>
 		</div>
 	);
 };
